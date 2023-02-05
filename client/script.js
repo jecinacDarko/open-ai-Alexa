51d59ad3dbbac6d40ecd945089f479cb5d27815e
Darko Jecinac <darkojecinac@gmail.com>
2023-02-05 12:22:08 +0100

    debugging

--- client/script.js
+++ client/script.js
@@ -15,7 +15,7 @@ function loader(element) {
     if(element.textContent === '....'){
       element.textContent ='';
     }
-  },300)
+  }, 300);
 }
 
 function typeText(element, text) {
@@ -23,7 +23,8 @@ function typeText(element, text) {
 
   let interval = setInterval(() => {
     if(index < text.length) {
-      element.innerHTML += text.charAt(index);
+      element.innerHTML += text.charAt(index)
+      index++
     } else {
       clearInterval(interval);
     }
@@ -40,20 +41,19 @@ function generateUniqueId() {
 
 function chatStripe (isAi, value, uniqueId) {
  return (
-  `
-  <div class="wrapper" ${isAi && 'ai'}">
-    <divc class="chat">
-      <div class="profile">
-        <img
-          src="${isAi ?  bot : user}"
-          alt="${isAi ? 'bot' : 'user'}"
-        />
-      </div>
-        <div class="message" id="${uniqueId}>${value}
+    `
+    <div class="wrapper" ${isAi && 'ai'}">
+      <div class="chat">
+        <div class="profile">
+          <img
+            src="${isAi ?  bot : user}"
+            alt="${isAi ? 'bot' : 'user'}"
+          />
+        </div>
+          <div class="message" id="${uniqueId}>${value}</div>
       </div>
     </div>
-  </div>
-  `
+    `
   )
 }
 
@@ -70,5 +70,39 @@ const handleSubmit = async (e) => {
 
   chatContainer.scrollTop = chatContainer.scrollHeight;
   const messageDiv = document.getElementById(uniqueId);
+ 
+  loader(messageDiv);
 
+  const response = await fetch('https://codex-im0y.onrender.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = " "
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
+
+        typeText(messageDiv, parsedData)
+    } else {
+        const err = await response.text()
+
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
+    }
 }
+
+
+form.addEventListener('submit' , handleSubmit); 
+form.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) {
+    handleSubmit(e); 
+  }
+})
