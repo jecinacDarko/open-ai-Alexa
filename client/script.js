995cb61bd93316ec8cb61e9b1daa6adfdd67342b
jecinacDarko <darkojecinac@gmail.com>
2023-02-07 12:57:01 +0100

    all working

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import bot from './assets/chatbot.png';
+import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
@@ -73,8 +73,7 @@ const handleSubmit = async (e) => {
  
   loader(messageDiv);
 
-    const response = await fetch('https://open-ai-alexa.onrender.com',
-    // const response = await fetch('https://open-ai-alexa.onrender.com', 
+    const response = await fetch('https://open-ai-alexa.onrender.com', 
     {
       method: 'POST',
       headers: {
